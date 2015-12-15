630ea1962f0b2f787da5ea6e15ade0dfe065091a
Snake1999 <237356812@qq.com>
2015-12-15 12:31:34 +0800

    Enhancements, Economy framework

---
+++ NuclearEconomy/src/com/fcmcpe/nuclear/economy/NuclearEconomyPlugin.java
@@ -0,0 +1,10 @@
+package com.fcmcpe.nuclear.economy;
+
+import cn.nukkit.plugin.PluginBase;
+
+/**
+ * Created on 2015/12/15 by xtypr.
+ * Package com.fcmcpe.nuclear.economy in project NuclearPlugins .
+ */
+public final class NuclearEconomyPlugin extends PluginBase {
+}
---
+++ NuclearLogin/src/com/fcmcpe/nuclear/login/event/NuclearLoginEvent.java
@@ -0,0 +1,11 @@
+package com.fcmcpe.nuclear.login.event;
+
+import cn.nukkit.event.Event;
+
+/**
+ * Created on 2015/12/15 by xtypr.
+ * Package com.fcmcpe.nuclear.login.event in project NuclearPlugins .
+ */
+public abstract class NuclearLoginEvent extends Event {
+
+}
